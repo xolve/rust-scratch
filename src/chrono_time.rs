0e8251160fd64668e81d3a249866d699dd00efc2
xolve <xolve@protonmail.com>
2022-08-18 10:29:56 +0530

    Continuing 9.6.3

--- src/chrono_time.rs
+++ src/chrono_time.rs
@@ -1,4 +1,30 @@
+use chrono::{DateTime, Local};
+use clap::{Arg, Command};
+
 fn main() {
-    let now = chrono::Local::now();
-    println!("{now:?}");
+    let app = Command::new("clock")
+        .arg(
+            Arg::with_name("command")
+                .takes_value(true)
+                .possible_values(["get", "set"])
+                .default_value("get"),
+        )
+        .arg(
+            Arg::with_name("format")
+                .long("use-standard")
+                .takes_value(true)
+                .default_value("timestamp"),
+        );
+}
+
+struct Clock;
+
+impl Clock {
+    fn get() -> DateTime<Local> {
+        Local::now()
+    }
+
+    fn set() -> ! {
+        todo!()
+    }
 }
