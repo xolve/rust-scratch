ab65c8311430a7cc381294ab3f6326fcae1083f0
xolve <xolve@protonmail.com>
2022-08-19 18:09:34 +0530

    Continue from Rust In Action 9.9.1

--- src/chrono_time.rs
+++ src/chrono_time.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Local, TimeZone};
 use clap::{Arg, Command};
 
 fn main() {
@@ -13,8 +13,30 @@ fn main() {
             Arg::with_name("format")
                 .long("use-standard")
                 .takes_value(true)
+                .possible_values(["rfc2822", "rfc3339", "timestamp"])
                 .default_value("timestamp"),
+        )
+        .arg(
+            Arg::with_name("datetime")
+               .help("When <action> is 'set' apply <datetime>, else ignore.")
         );
+
+        let args = app.get_matches();
+
+        let command = args.value_of("command").unwrap();
+        let format = args.value_of("format").unwrap();
+
+        if command == "set" {
+            todo!()
+        }
+
+        let now = Clock::get();
+        match format {
+            "timestamp" => println!("{}", now.timestamp()),
+            "rfc2822" => println!("{}", now.to_rfc2822()),
+            "rfc3339" => println!("{}", now.to_rfc3339()),
+            _ => unreachable!(),
+        }
 }
 
 struct Clock;
